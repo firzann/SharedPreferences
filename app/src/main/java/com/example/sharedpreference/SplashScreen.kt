49fbd4b16272234ff70567cf7544c7ed702bf3c8
firzann <firzan.a@yahoo.com>
2021-10-26 15:24:33 +0700

    nambah SplashScreen

--- app/src/main/java/com/example/sharedpreference/SplashScreen.kt
+++ app/src/main/java/com/example/sharedpreference/SplashScreen.kt
@@ -1,11 +1,26 @@
 package com.example.sharedpreference
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.Window
 
 class SplashScreen : AppCompatActivity() {
+
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //menghilangkan ActionBar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_splash_screen)
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000L) //3000 L = 3 detik
     }
 }
